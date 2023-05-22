2e68e7455631de0cec8ee3fed72c98f89c3ba483
zahar <45snik45@gmail.com>
2023-05-22 17:40:20 +0300

    das

--- src/components/SimpleTrack.tsx
+++ src/components/SimpleTrack.tsx
@@ -4,25 +4,25 @@ import { useAppDispatch, useAppselector } from "../store/store";
 import { setCurentTrack } from "../store/TrackSlice";
 
 import jpg1 from "../assets/images/beat1.jpg";
-import jpg2 from "../assets/images/beat2.jpg";
-import jpg3 from "../assets/images/beat3.jpg";
-import jpg4 from "../assets/images/beat4.jpg";
+// import jpg2 from "../assets/images/beat2.jpg";
+// import jpg3 from "../assets/images/beat3.jpg";
+// import jpg4 from "../assets/images/beat4.jpg";
 import sprite from "../assets/images/sprite.svg";
 
 const SimpleTrack: FC<IData> = (props) => {
-  const { genre, group, track, dur, id } = props;
+  const { genre, group, track, dur } = props;
 
   const disp = useAppDispatch();
   const { curentTrack, isPlaying } = useAppselector(
     (state) => state.inputreduser
   );
 
-  const imagesarr = [jpg1, jpg2, jpg3, jpg4];
+  //   const imagesarr = [jpg1, jpg2, jpg3, jpg4];
 
   return (
     <div className="tarck" onClick={() => disp(setCurentTrack(props.id))}>
       <div className="tarck__img">
-        <img src={imagesarr[id]} alt="tack img" />
+        <img src={jpg1} />
       </div>
       <div className="tarck__info">
         <div className="tarck__author">{group}</div>
